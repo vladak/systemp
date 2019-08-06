61d74b2c6c52c67a99bbd88420ffb81d56382a23
Vladimir Kotal <vlada@devnull.cz>
2019-08-06 22:35:07 +0200

    initial checkin

---
+++ run.sh
@@ -0,0 +1,4 @@
+. ./venv/bin/activate
+export FLASK_APP=systemp.py
+# export FLASK_DEBUG=1
+flask run
---
+++ systemp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    mydict = {}
+
+    with open('/sys/class/thermal/thermal_zone0/temp') as f:
+        temp = float(f.read()) / 1000
+        mydict['cpu'] = temp
+
+    return mydict
+
+
+if __name__ == "__main__":
+    print(index())

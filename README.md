eceae07072480e788a0c15556a3f811d55a27084
Vladimir Kotal <vlada@kotalovi.cz>
2025-07-13 20:58:38 +0200

    publish metrics to MQTT
    fixes #2

--- README.md
+++ README.md
@@ -18,6 +18,18 @@ python3 -m venv env
 . ./env/bin/activate
 python3 -m pip install -r requirements.txt
 ```
+
+## Setup
+
+Create `/srv/systemp/environment` file with the arguments:
+```
+cat << EOF >/srv/systemp/environment
+ARGS=--mqtt_hostname example.com --mqtt_topic foo/bar
+EOF
+```
+
+## Start
+
 - copy `systemp.service` file to `/etc/systemd/system/systemp.service`:
 ```
 sudo cp /srv/systemp/systemp.service /etc/systemd/system/systemp.service
@@ -37,17 +49,49 @@ sudo systemctl status systemp
 ```
 
 ## Use
-```
-$ curl http://localhost:8222/
 
-...
-
-CPU_temp_pihole 58.913
-```
-
-This can be now added to `/etc/prometheus/prometheus.yml` like so:
+Setup MQTT Prometheus exporter (see https://github.com/vladak/weather),
+add the following snippet to `/etc/prometheus/mqtt-exporter.yaml` under the
+`metrics`:
 ```yml
-  - job_name: CPU_pihole
-    static_configs:
-      - targets: ['pi:8222']
+  -
+    # The name of the metric in prometheus
+    prom_name: sys_temperature
+    # The name of the metric in a MQTT JSON message
+    mqtt_name: sys_temperature
+    # The prometheus help text for this metric
+    help: temperature reading
+    # The prometheus type for this metric. Valid values are: "gauge" and "counter"
+    type: gauge
+    # A map of string to string for constant labels. This labels will be attached to every prometheus metric
+    #const_labels:
+    #  sensor_type: dht22
 ```
+
+Assumes labeling done in Prometheus config `/etc/prometheus/prometheus.yml`:
+
+```yml
+  - job_name: mqtt
+    # If prometheus-mqtt-exporter is installed, grab metrics from external sensors.
+    static_configs:
+      - targets: ['localhost:9641']
+    # The MQTT based sensor publish the data only now and then.
+    scrape_interval: 1m
+    # Add the location as a tag.
+    metric_relabel_configs:
+     - source_labels: [topic]
+       target_label: location
+       regex: 'devices/([[:alnum:]]*)/[[:alnum:]]*'
+       action: replace
+       replacement: "$1"
+     - source_labels: [topic]
+       target_label: name
+       regex: 'devices/[[:alnum:]]*/([[:alnum:]]*)'
+       action: replace
+       replacement: "$1"
+```
+
+## Links
+
+- https://betterstack.com/community/guides/monitoring/prometheus-relabeling/
+
--- systemp.py
+++ systemp.py
@@ -1,43 +1,111 @@
 #!/usr/bin/env python3
 """
-Acquire CPU temperature and present it via HTTP for Prometheus server.
+Acquire CPU temperature and publish it to MQTT.
 """
 
+import argparse
+import json
 import logging
+import socket
+import ssl
 import sys
 import time
-import socket
-from prometheus_client import start_http_server, Gauge
+
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+from adafruit_minimqtt.adafruit_minimqtt import MMQTTException
+
+from logutil import LogLevelAction
 
 
-EXPOSED_PORT = 8222   # port to listen on for HTTP requests
+def parse_args():
+    """
+    Parse command line arguments
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Publish system temperature to MQTT",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--mqtt_hostname",
+        help="MQTT broker hostname",
+        required=True,
+    )
+    parser.add_argument(
+        "-l",
+        "--loglevel",
+        action=LogLevelAction,
+        help='Set log level (e.g. "ERROR")',
+        default=logging.INFO,
+    )
+    parser.add_argument(
+        "-t",
+        "--timeout",
+        help="Timeout in seconds to sleep between publishing",
+        default=5,
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="MQTT broker port",
+        default=1883,
+    )
+    parser.add_argument(
+        "--mqtt_topic",
+        help="MQTT topic to publish to",
+        required=True,
+    )
+
+    return parser.parse_args()
 
 
 def main():
+    """
+    read system temperature and publish to MQTT in a cycle
+    """
+    args = parse_args()
+
+    logging.basicConfig()
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(args.loglevel)
 
-    logger.info("Listening on port " + str(EXPOSED_PORT))
-    start_http_server(EXPOSED_PORT)
-
-    sleep_seconds = 5
-
-    hostname = socket.gethostname().split(".")[0]
-    g = Gauge('CPU_temp_' + hostname, 'CPU temperature on ' + hostname)
+    mqtt = MQTT.MQTT(
+        broker=args.mqtt_hostname,
+        port=args.mqtt_port,
+        socket_pool=socket,
+        ssl_context=ssl.create_default_context(),
+    )
+    logger.info(f"Connecting to MQTT broker {args.mqtt_hostname} on port {args.mqtt_port}")
+    mqtt.connect()
 
     while True:
-        with open('/sys/class/thermal/thermal_zone0/temp') as f:
-            val = f.read()
+        # Make sure to stay connected to the broker e.g. in case of keep alive.
+        try:
+            mqtt.loop(1)
+        except MMQTTException as mqtt_exc:
+            logger.warning(f"Got MQTT exception: {mqtt_exc}")
+            mqtt.reconnect()
+
+        with open(
+            "/sys/class/thermal/thermal_zone0/temp", encoding="ascii"
+        ) as file_handle:
+            val = file_handle.read()
             if len(val) == 0:
                 logger.debug("Empty read")
                 continue
 
-        temp = float(val) / 1000
-        logger.debug(temp)
-        g.set(temp)
+        temp_val = float(val) / 1000
+        logger.debug(f"Current system temperature: {temp_val}")
+        mqtt_payload_dict = {"sys_temperature": temp_val}
 
-        logger.debug("Sleeping for " + str(sleep_seconds) + " seconds")
-        time.sleep(sleep_seconds)
+        try:
+            mqtt.publish(args.mqtt_topic, json.dumps(mqtt_payload_dict))
+        except MMQTTException as mqtt_exc:
+            logger.warning(f"Got MQTT exception: {mqtt_exc}")
+            mqtt.reconnect()
+
+        logger.debug(f"Sleeping for {args.timeout} seconds")
+        time.sleep(args.timeout)
 
 
 if __name__ == "__main__":
